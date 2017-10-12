a78739abac285cd4031749ba6a128d3c252f43c3
lisen <suilei@agora.io>
2017-10-12 15:07:53 +0800

    "autoPush"

---
+++ cp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "hello lisen! this is your cp shell script"
+read -p "input DevDir:" DevDir
+read -p "input GithubDir:" GithubDir
+rm -r $GithubDir
+cp -r $DevDir $GithubDir
+cd $GithubDir
+git pull
+git status
+while true
+do
+    read -p "input adding file:" file
+    if  [ ! -n "$file" ] ;then
+        echo "Adding file end"
+        break
+    else
+        git add $file
+git status
+fi
+done
+git status
+read -p "input commitString:" commitString
+git commit -m $commitString
+read -p "input remote branch:" remote_branch
+git push origin $remote_branch

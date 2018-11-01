21a123eb57bb6e793c0f88c2a5704eb9e0d91f0c
lisen <suilei@agora.io>
2018-11-01 16:45:41 +0800

    add del downloadPackage for windows

--- installDemo/installApplication.py
+++ installDemo/installApplication.py
@@ -190,7 +190,10 @@ def clickMeInstallChrome():  # 当acction被点击时,该函数则生效
             except:
                 tkinter.messagebox.showerror('错误', '安装失败')
                 # installChromeAction.configure(text='Install fail ')
-            os.system("rm -f %s" % packagePWD)
+            if platform.system() == "Windows":
+                os.system("rm -f %s" % packagePWD)
+            else:
+                os.system("del %s" % packagePWD)
         else:
             try:
                 if platform.system() == "Windows":

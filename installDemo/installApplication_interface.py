ec91f3646a7b959aa0516165d7c3e51125701c6d
uncleSmile <974267518@qq.com>
2018-11-01 18:43:07 +0800

    add install and uninstall on mac interface

--- installDemo/installApplication_interface.py
+++ installDemo/installApplication_interface.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os,re,sys
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox
+import os,re,sys,platform
 if sys.version < "3":
     import urllib2
 else:
@@ -33,19 +36,33 @@ firefox = {
     "64":r".\Firefox64.0b5.exe"
 }
 
-def execInstallCMD(pwd):
+mac_chrome = {
+    "49":r".\49.0.2623.13_chrome64_dev_osx_installer.dmg"
+}
+mac_firefox = {
+    "56":r".\firefox\Firefox56.0.dmg",
+    "57":r".\firefox\Firefox57.0.dmg",
+    "58":r".\firefox\Firefox58.0.dmg",
+    "59":r".\firefox\Firefox59.0.dmg",
+    "60":r".\firefox\Firefox60.0.dmg",
+    "61":r".\firefox\Firefox61.0.dmg",
+    "62":r".\firefox\Firefox62.0.dmg",
+    "63":r".\firefox\Firefox63.0.dmg",
+    "64":r".\firefox\Firefox64.0b5.dmg"
+}
+
+def execInstallWinCMD(pwd):
     cmd = "start /wait "+pwd+" /S"
     f = os.system(cmd)
     if str(f) != "0":
         return False
 
-# install function
-def install_function(browserPackage):
+def install_on_win(browserPackage):
     if browserPackage != "":
         if re.match("http",browserPackage):
             packagePWD = downloadPackage(browserPackage)
             try:
-                state = execInstallCMD(packagePWD)
+                state = execInstallWinCMD(packagePWD)
                 if state == False:
                     print("use http way to install failed")
                 else:
@@ -54,7 +71,7 @@ def install_function(browserPackage):
                 print("use http way to install failed ")
         else:
             try:
-                state = execInstallCMD(browserPackage)
+                state = execInstallWinCMD(browserPackage)
                 if state == False:
                     print("use location station to install failed")
                 else:
@@ -66,7 +83,7 @@ def install_function(browserPackage):
             versionPWD = chrome[str(browserPackage)]
             if os.path.exists(versionPWD):
                 try:
-                    state = execInstallCMD(versionPWD)
+                    state = execInstallWinCMD(versionPWD)
                     if state == False:
                         print("use exe to install failed")
                     else:
@@ -93,21 +110,145 @@ def uninstall_chrome():
 
 def uninstall_firefox():
     cmd = r'"C:\Program Files\Mozilla Firefox\uninstall\helper.exe" /S'
+    print("exec:", cmd)
     f = os.system(cmd)
     if str(f) != "0":
         return False
     else:
         print("firfox uninstall success")
 
+################### MAC ###################################################
+
+def install_chrome_on_mac(browserPackage):
+    if browserPackage != "":
+        if re.match("http",browserPackage):
+            packagePWD = downloadPackage(browserPackage)
+            try:
+                state = exec_install_cmd_on_mac(packagePWD,Btype=Chrome)
+                if state == False:
+                    print("use http way to install failed")
+                else:
+                    print("use http way to install success")
+            except:
+                print("use http way to install failed ")
+        else:
+            try:
+                state = exec_install_cmd_on_mac(browserPackage,Btype=Chrome)
+                if state == False:
+                    print("use location station to install failed")
+                else:
+                    print("use location station to install success")
+            except:
+                print("use location station to install failed")
+    else:
+        if str(browserPackage) in mac_chrome:
+            versionPWD = mac_chrome[str(browserPackage)]
+            if os.path.exists(versionPWD):
+                try:
+                    state = exec_install_cmd_on_mac(versionPWD,Btype=Chrome)
+                    if state == False:
+                        print("use exe to install failed")
+                    else:
+                        print("use exe to install successed")
+                except:
+                    print("use exe to install failed")
+            else:
+                print("This version of the file does not exist")
+        else:
+            print("The dict is missing this value")
+
+def install_firefox_on_mac(browserPackage):
+    if browserPackage != "":
+        if re.match("http",browserPackage):
+            packagePWD = downloadPackage(browserPackage)
+            try:
+                state = exec_install_cmd_on_mac(packagePWD,Btype=Firefox)
+                if state == False:
+                    print("use http way to install failed")
+                else:
+                    print("use http way to install success")
+            except:
+                print("use http way to install failed ")
+        else:
+            try:
+                state = exec_install_cmd_on_mac(browserPackage,Btype=Firefox)
+                if state == False:
+                    print("use location station to install failed")
+                else:
+                    print("use location station to install success")
+            except:
+                print("use location station to install failed")
+    else:
+        if str(browserPackage) in mac_firefox:
+            versionPWD = mac_firefox[str(browserPackage)]
+            if os.path.exists(versionPWD):
+                try:
+                    state = exec_install_cmd_on_mac(versionPWD,Btype=Firefox)
+                    if state == False:
+                        print("use exe to install failed")
+                    else:
+                        print("use exe to install successed")
+                except:
+                    print("use exe to install failed")
+            else:
+                print("This version of the file does not exist")
+        else:
+            print("The dict is missing this value")
+
+def exec_install_cmd_on_mac(pwd,Btype):
+    attachCMD = "hdiutil attach " + pwd
+    out = os.system(attachCMD)
+    if Btype == "Chrome":
+        os.system("rm -rf /Applications/Google\ Chrome.app/")
+        os.system("cp -r /Volumes/Google\ Chrome/Google\ Chrome.app/ /Applications/Google\ Chrome.app/")
+        os.system("hdiutil detach /Volumes/Google\ Chrome/")
+        print ("install Chrome success ")
+    elif Btype == "Firefox":
+        os.system("rm -rf /Applications/Firefox/")
+        os.system("cp -r /Volumes/Firefox/Firefox.app/ /Applications/Firefox.app/")
+        os.system("hdiutil detach /Volumes/Firefox/")
+        print ("install Firefox success")
+    else:
+        print("unsupport this browser to install")
+
+def uninstall_chrome_on_mac():
+    cmd = r'"rm -rf /Applications/Google\ Chrome.app'
+    f = os.system(cmd)
+    if str(f) != "0":
+        return False
+
+def uninstall_firefox_on_mac():
+    cmd = r'"rm -rf /Applications/Firefox.app/'
+    f = os.system(cmd)
+    if str(f) != "0":
+        return False
+
 def downloadPackage(url):
     # http://10.80.0.160:8888/Firefox60.0.exe
+    # packageName = url.split("/")[-1]
+    # packagePWD = os.getcwd() + "\\" + packageName
+    # if sys.version < "3":
+    #     f = urllib2.urlopen(url)
+    #     data = f.read()
+    #     with open(packageName, "wb") as code:
+    #         code.write(data)
+    # else:
+    #     request.urlretrieve(url,"./%s"%packageName)
+    # return packagePWD
     packageName = url.split("/")[-1]
-    packagePWD = os.getcwd() + "\\" + packageName
+    if platform.system() == "Windows":
+        packagePWD = os.getcwd() + "\\" + packageName
+    else:
+        packagePWD = "/tmp/" + packageName
+        # packagePWD = os.getcwd() + "/" + packageName
     if sys.version < "3":
         f = urllib2.urlopen(url)
         data = f.read()
         with open(packageName, "wb") as code:
             code.write(data)
     else:
-        request.urlretrieve(url,"./%s"%packageName)
+        if platform.system() == "Windows":
+            request.urlretrieve(url, "./%s" % packageName)
+        else:
+            request.urlretrieve(url,"%s"%packagePWD)
     return packagePWD

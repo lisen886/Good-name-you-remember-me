edd7451d2dfd95ca82682a8383844251d15a4c43
uncleSmile <974267518@qq.com>
2018-11-02 17:14:10 +0800

    fix install on win

--- installDemo/installApplication_interface.py
+++ installDemo/installApplication_interface.py
@@ -9,47 +9,8 @@ if sys.version < "3":
 else:
     # 因为打包程序没法使用urllib，改用urllib2
     from urllib import request
-chrome = {
-    "49":r".\chrome\49.0.2623.75_chrome64_stable_windows_installer.exe",
-    "58":r".\58.0.3029.81_chrome64_stable_windows_installer.exe",
-    "59":r".\59.0.3071.25_chrome64_dev_windows_installer.exe",
-    "60":r".\60.0.3112.113_chrome64_stable_windows_installer.exe",
-    "61":r".\61.0.3163.79_chrome64_stable_windows_installer.exe",
-    "62":r".\62.0.3202.62_chrome64_stable_windows_installer.exe",
-    "63":r".\63.0.3239.132_chrome64_stable_windows_installer.exe",
-    "64":r".\64.0.3282.140_chrome64_stable_windows_installer.exe",
-    "65":r".\65.0.3325.162_chrome64_stable_windows_installer.exe",
-    "66":r".\66.0.3359.139_chrome64_stable_windows_installer.exe",
-    "67":r".\67.0.3396.87_chrome64_stable_windows_installer.exe",
-    "68":r".\68.0.3440.106_chrome64_stable_windows_installer.exe",
-    "69":r".\chrome\69.0.3497.81_chrome64_stable_windows_installer.exe"
-}
-firefox = {
-    "56":r".\Firefox56.0.exe",
-    "57":r".\Firefox57.0.exe",
-    "58":r".\Firefox58.0.exe",
-    "59":r".\Firefox59.0.exe",
-    "60":r".\Firefox60.0.exe",
-    "61":r".\Firefox61.0.exe",
-    "62":r".\Firefox62.0.exe",
-    "63":r".\Firefox63.0.exe",
-    "64":r".\Firefox64.0b5.exe"
-}
 
-mac_chrome = {
-    "49":r"./49.0.2623.13_chrome64_dev_osx_installer.dmg"
-}
-mac_firefox = {
-    "56":r"./Firefox56.0.dmg",
-    "57":r"./Firefox57.0.dmg",
-    "58":r"./Firefox58.0.dmg",
-    "59":r"./Firefox59.0.dmg",
-    "60":r"./Firefox60.0.dmg",
-    "61":r"./Firefox61.0.dmg",
-    "62":r"./Firefox62.0.dmg",
-    "63":r"./Firefox63.0.dmg",
-    "64":r"./Firefox64.0b5.dmg"
-}
+#-------------------------------   Windows   ---------------------------------------#
 
 def execInstallWinCMD(pwd):
     cmd = "start /wait "+pwd+" /S"
@@ -57,45 +18,76 @@ def execInstallWinCMD(pwd):
     if str(f) != "0":
         return False
 
-def install_on_win(browserPackage):
-    if browserPackage != "":
-        if re.match("http",browserPackage):
-            packagePWD = downloadPackage(browserPackage)
-            try:
-                state = execInstallWinCMD(packagePWD)
-                if state == False:
-                    print("use http way to install failed")
-                else:
-                    print("use http way to install success")
-            except:
-                print("use http way to install failed ")
-        else:
-            try:
-                state = execInstallWinCMD(browserPackage)
-                if state == False:
-                    print("use location station to install failed")
-                else:
-                    print("use location station to install success")
-            except:
-                print("use location station to install failed")
-    else:
-        if str(browserPackage) in chrome:
-            versionPWD = chrome[str(browserPackage)]
-            if os.path.exists(versionPWD):
-                try:
-                    state = execInstallWinCMD(versionPWD)
-                    if state == False:
-                        print("use exe to install failed")
-                    else:
-                        print("use exe to install successed")
-                except:
-                    print("use exe to install failed")
+def install_chrome_on_win(browserPackage,useDicTag=False):
+    if re.match("http",browserPackage):
+        packagePWD = downloadPackage(browserPackage)
+        try:
+            state = execInstallWinCMD(packagePWD)
+            if state == False:
+                print("use http way to install failed")
             else:
-                print("This version of the file does not exist")
-        else:
-            print("The dict is missing this value")
+                print("use http way to install success")
+        except:
+            print("use http way to install failed ")
+    elif useDicTag is True :
+        versionPWD = getJsonData("winchromeList")[browserPackage]
+        print(versionPWD)
+        # if str(browserPackage) in macChromeDict:
+        try:
+            pwd = os.getcwd()
+            cmd = "start /wait " + pwd + versionPWD + " /S"
+            f = os.system(cmd)
+            if str(f) != "0":
+                print("use exe to install failed")
+            else:
+                print("use exe to install successed")
+        except:
+            print("use exe to install failed")
+    else:
+        try:
+            state = execInstallWinCMD(browserPackage)
+            if state == False:
+                print("use location station to install failed")
+            else:
+                print("use location station to install success")
+        except:
+            print("use location station to install failed")
 
-def uninstall_chrome():
+def install_firefox_on_win(browserPackage,useDicTag=False):
+    if re.match("http",browserPackage):
+        packagePWD = downloadPackage(browserPackage)
+        try:
+            state = execInstallWinCMD(packagePWD)
+            if state == False:
+                print("use http way to install failed")
+            else:
+                print("use http way to install success")
+        except:
+            print("use http way to install failed ")
+    elif useDicTag is True :
+        versionPWD = getJsonData("winfirefoxList")[browserPackage]
+        # if str(browserPackage) in macChromeDict:
+        try:
+            pwd = os.getcwd()
+            cmd = "start /wait " + pwd + versionPWD + " /S"
+            f = os.system(cmd)
+            if str(f) != "0":
+                print("use exe to install failed")
+            else:
+                print("use exe to install successed")
+        except:
+            print("use exe to install failed")
+    else:
+        try:
+            state = execInstallWinCMD(browserPackage)
+            if state == False:
+                print("use location station to install failed")
+            else:
+                print("use location station to install success")
+        except:
+            print("use location station to install failed")
+
+def uninstall_chrome_on_win():
     os.system("taskkill /F /IM chrome.exe /T")
     get_version_cmd = r'wmic datafile where name="C:\\Users\\%Username%\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe" get Version /value'
     f = os.popen(get_version_cmd)
@@ -108,16 +100,15 @@ def uninstall_chrome():
     if str(f) != "0" and str(f) != "19":
         return False
 
-def uninstall_firefox():
+def uninstall_firefox_on_win():
     cmd = r'"C:\Program Files\Mozilla Firefox\uninstall\helper.exe" /S'
-    print("exec:", cmd)
     f = os.system(cmd)
     if str(f) != "0":
         return False
     else:
         print("firfox uninstall success")
 
-################### MAC ###################################################
+#-------------------------------   MAC ---------------------------------------#
 
 def install_chrome_on_mac(browserPackage,UseDicTag=False):
     if re.match("http",browserPackage):
@@ -250,15 +241,9 @@ def downloadPackage(url):
             request.urlretrieve(url,"%s"%packagePWD)
     return packagePWD
 
-# def getJsonData(browserType,version):
-#     with open("./browserConfig.json","r") as load_f:
-#         load_dict = json.load(load_f)
-#         return load_dict[browserType],load_dict[browserType][str(version)]
-
 def getJsonData(browserType):
     with open("./browserConfig.json","r") as load_f:
         load_dict = json.load(load_f)
-        # print(load_dict[browserType]["49"])
         return load_dict[browserType]
 if __name__ == "__main__":
     print("test interface")
@@ -266,10 +251,16 @@ if __name__ == "__main__":
     # install_chrome_on_mac("/tmp/49.0.2623.13_chrome64_dev_osx_installer.dmg")
     # install_chrome_on_mac(browserPackage="49",UseDicTag=True)
     # uninstall_chrome_on_mac()
-    install_firefox_on_mac("http://10.80.0.160:8888/AgoraMacInstall/firefox/Firefox63.0.dmg")
+    # install_firefox_on_mac("http://10.80.0.160:8888/AgoraMacInstall/firefox/Firefox63.0.dmg")
     # install_firefox_on_mac("/tmp/Firefox63.0.dmg")
     # uninstall_firefox_on_mac()
-    # print(getJsonData("macchromeList")["49"])
-    # print("59" in str(getJsonData("winchromeList","59")))
 
-    # print(getJsonData("macchromeList", str(49)))
+    # uninstall_chrome_on_win()
+    # install_chrome_on_win("http://10.80.0.160:8888/AgoraWinInstall/chrome/69.0.3497.81_chrome64_stable_windows_installer.exe")
+    # install_chrome_on_win("D:\\test_install\\69.0.3497.81_chrome64_stable_windows_installer.exe")
+    # install_chrome_on_win("69",useDicTag=True)
+
+    # uninstall_firefox_on_win()
+    # install_firefox_on_win("http://10.80.0.160:8888/AgoraWinInstall/firefox/Firefox63.0.exe")
+    # install_firefox_on_win("60",useDicTag=True)
+    install_firefox_on_win("D:\\test_install\\FirefoxSetup64.0b4.exe")

ed76739f39968a9b7fa06b6c01b5900e8c087d70
lisen <suilei@agora.io>
2019-03-14 17:49:15 +0800

    update macBrowserCpuMem.py

--- autoTest/macBrowserCpuMem.py
+++ autoTest/macBrowserCpuMem.py
@@ -30,7 +30,7 @@ def getCPU(GPUpid,Capturepid,premiumpid):
     premiumCPU = premiump.cpu_percent(None)
     logger.info("premiumCPU is %d%%" %(premiumCPU))
     webRTCCPU = GPUCPU+CaptureCPU+premiumCPU
-    return webRTCCPU/2
+    return webRTCCPU/2,GPUCPU/2,CaptureCPU/2,premiumCPU/2
 
 def getMEM(GPUpid,Capturepid,premiumpid):
     mem = psutil.virtual_memory()
@@ -42,10 +42,13 @@ def getMEM(GPUpid,Capturepid,premiumpid):
     premiumMEM = premiump.memory_percent()
     memUSED = mem.used
     webRTCMEM = (((GPUMEM+CaptureMEM+premiumMEM)/100)*(memUSED))/1024/1024
-    logger.info("GPU MEM is %dMB"% ((((GPUMEM)/100)*(memUSED))/1024/1024))
-    logger.info("Capture MEM is %sMB"% ((((CaptureMEM)/100)*(memUSED))/1024/1024))
-    logger.info("Premium MEM is %sMB"% ((((premiumMEM)/100)*(memUSED))/1024/1024))
-    return webRTCMEM
+    GPUMEM = (((GPUMEM)/100)*(memUSED))/1024/1024
+    CaptureMEM = (((CaptureMEM)/100)*(memUSED))/1024/1024
+    premiumMEM = (((premiumMEM)/100)*(memUSED))/1024/1024
+    logger.info("GPU MEM is %dMB"% GPUMEM)
+    logger.info("Capture MEM is %sMB"% CaptureMEM)
+    logger.info("Premium MEM is %sMB"% premiumMEM)
+    return webRTCMEM,GPUMEM,CaptureMEM,premiumMEM
 
 def getSafariCPU(safariProcesspid,premiumpid):
     safarip = psutil.Process(safariProcesspid)
@@ -59,7 +62,7 @@ def getSafariCPU(safariProcesspid,premiumpid):
     premiumCPU = premiump.cpu_percent(None)
     logger.info("premiumCPU is %d%%" % (premiumCPU))
     webRTCCPU = safariProcessCPU + premiumCPU
-    return webRTCCPU / 2
+    return webRTCCPU / 2,safariProcessCPU/2,premiumCPU/2
 
 def getSafariMEM(safariProcesspid,premiumpid):
     mem = psutil.virtual_memory()
@@ -72,44 +75,79 @@ def getSafariMEM(safariProcesspid,premiumpid):
     premiumMEM = premiump.memory_percent()
 
     webRTCMEM = (((safariProcessMEM + premiumMEM) / 100) * (memUSED)) / 1024 / 1024
-    logger.info("safariProcess MEM is %dMB" % ((((safariProcessMEM) / 100) * (memUSED)) / 1024 / 1024))
-    logger.info("Premium MEM is %sMB" % ((((premiumMEM) / 100) * (memUSED)) / 1024 / 1024))
-    return webRTCMEM
+    safariProcessMEM = ((((safariProcessMEM) / 100) * (memUSED)) / 1024 / 1024)
+    premiumMEM = ((((premiumMEM) / 100) * (memUSED)) / 1024 / 1024)
+    logger.info("safariProcess MEM is %dMB" % safariProcessMEM)
+    logger.info("Premium MEM is %sMB" % premiumMEM)
+    return webRTCMEM,safariProcessMEM,premiumMEM
 
-def getChromeCPUMEM(secs,GPUpid, Capturepid, premiumpid):
+def getChromeCPUMEM(secs,frequency,GPUpid, Capturepid, premiumpid):
     cpuTotal = 0
     memTotal = 0
-    for i in range(0,10):
+    GPUCPUTotal = 0
+    CaptureCPUTotal = 0
+    premiumCPUTotal = 0
+    GPUMEMTotal = 0
+    CaptureMEMTotal = 0
+    premiumMEMTotal = 0
+    for i in range(0,frequency):
         time.sleep(secs)
-        cpu = getCPU(GPUpid, Capturepid, premiumpid)
+        webRTCCPU , GPUCPU, CaptureCPU, premiumCPU = getCPU(GPUpid, Capturepid, premiumpid)
+        logger.info("*******************第%d组 webRTC_CPU is %.2f%% *******************" % ((i+1),webRTCCPU))
+        webRTCMEM, GPUMEM, CaptureMEM, premiumMEM = getMEM(GPUpid, Capturepid, premiumpid)
+        logger.info("===================第%d组 webRTC_MEM is %.2fMB ===================" % ((i+1),webRTCMEM))
+        cpuTotal += webRTCCPU
+        memTotal += webRTCMEM
+        GPUCPUTotal += GPUCPU
+        CaptureCPUTotal += CaptureCPU
+        premiumCPUTotal += premiumCPU
+        GPUMEMTotal += GPUMEM
+        CaptureMEMTotal += CaptureMEM
+        premiumMEMTotal += premiumMEM
+    logger.info("GPUCPU平均值为：%.2f"%(GPUCPUTotal/frequency))
+    logger.info("CaptureCPU平均值为：%.2f"%(CaptureCPUTotal/frequency))
+    logger.info("premiumCPU平均值为：%.2f"%(premiumCPUTotal/frequency))
+    logger.info("GPU内存平均值为：%.2f"%(GPUMEMTotal/frequency))
+    logger.info("Capture内存平均值为：%.2f"%(CaptureMEMTotal/frequency))
+    logger.info("premium内存平均值为：%.2f"%(premiumMEMTotal/frequency))
+    logger.info("****************cpu平均值为：%.2f****************" % (cpuTotal / frequency))
+    logger.info("****************内存平均值为：%.2f****************" % (memTotal / frequency))
+
+
+def getSafariCPUMEM(secs,frequency,safariProcesspid,premiumpid):
+    cpuTotal = 0
+    memTotal = 0
+    safariProcessCPUTotal = 0
+    premiumCPUTotal = 0
+    safariProcessMEMTotal = 0
+    premiumMEMTotal = 0
+    for i in range(0,frequency):
+        time.sleep(secs)
+        cpu,safariProcessCPU,premiumCPU = getSafariCPU(safariProcesspid,premiumpid)
         logger.info("*******************第%d组 webRTC_CPU is %.2f%% *******************" % ((i+1),cpu))
-        mem = getMEM(GPUpid, Capturepid, premiumpid)
+        mem,safariProcessMEM,premiumMEM = getSafariMEM(safariProcesspid,premiumpid)
         logger.info("===================第%d组 webRTC_MEM is %.2fMB ===================" % ((i+1),mem))
         cpuTotal += cpu
         memTotal += mem
-    logger.info("cpu平均值为：%.2f"%(cpuTotal/10))
-    logger.info("内存平均值为：%.2f"%(memTotal/10))
+        safariProcessCPUTotal += safariProcessCPU
+        premiumCPUTotal += premiumCPU
+        safariProcessMEMTotal += safariProcessMEM
+        premiumMEMTotal += premiumMEM
 
-def getSafariCPUMEM(secs,safariProcesspid,premiumpid):
-    cpuTotal = 0
-    memTotal = 0
-    for i in range(0,10):
-        time.sleep(secs)
-        cpu = getSafariCPU(safariProcesspid,premiumpid)
-        logger.info("*******************第%d组 webRTC_CPU is %.2f%% *******************" % ((i+1),cpu))
-        mem = getSafariMEM(safariProcesspid,premiumpid)
-        logger.info("===================第%d组 webRTC_MEM is %.2fMB ===================" % ((i+1),mem))
-        cpuTotal += cpu
-        memTotal += mem
-    logger.info("cpu平均值为：%.2f"%(cpuTotal/10))
-    logger.info("内存平均值为：%.2f"%(memTotal/10))
+    logger.info("safariProcessCPU平均值为：%.2f"%(safariProcessCPUTotal/frequency))
+    logger.info("premiumCPU平均值为：%.2f"%(premiumCPUTotal/frequency))
+    logger.info("safariProcess内存平均值为：%.2f"%(safariProcessMEMTotal/frequency))
+    logger.info("premium内存平均值为：%.2f"%(premiumMEMTotal/frequency))
+    logger.info("****************cpu平均值为：%.2f****************" % (cpuTotal / frequency))
+    logger.info("****************内存平均值为：%.2f****************" % (memTotal / frequency))
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 5:
         logger.error (
-            '''\nrun as: python3 macBrowserCpuMem.py browserType secs *pid
+            '''\nrun as: python3 macBrowserCpuMem.py browserType secs frequency *pid
                 browserType:chrome or safari
                 secs:sleep time
+                frequency:run times
                 *pid:
                     chrome:GPUpid Capturepid premiumpid
                     safari:safariProcesspid premiumpid
@@ -118,12 +156,14 @@ if __name__ == '__main__':
     browserType = sys.argv[1]
     if browserType == "chrome":
         secs = int(sys.argv[2])
-        GPUpid = int(sys.argv[3])
-        Capturepid = int(sys.argv[4])
-        premiumpid = int(sys.argv[5])
-        getChromeCPUMEM(secs,GPUpid, Capturepid, premiumpid)
+        frequency = int(sys.argv[3])
+        GPUpid = int(sys.argv[4])
+        Capturepid = int(sys.argv[5])
+        premiumpid = int(sys.argv[6])
+        getChromeCPUMEM(secs,frequency, GPUpid, Capturepid, premiumpid)
     elif browserType == "safari":
         secs = int(sys.argv[2])
-        safariProcesspid = int(sys.argv[3])
-        premiumpid = int(sys.argv[4])
-        getSafariCPUMEM(secs, safariProcesspid, premiumpid)
+        frequency = int(sys.argv[3])
+        safariProcesspid = int(sys.argv[4])
+        premiumpid = int(sys.argv[5])
+        getSafariCPUMEM(secs, frequency, safariProcesspid, premiumpid)
